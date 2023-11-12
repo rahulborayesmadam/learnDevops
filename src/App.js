cb3d2c0e5bb211bfdf5783e54d9eaa0958233840
rahul bora <rahul.bora@yesmadam.com>
2023-11-12 10:46:02 +0530

    chagned text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn Devops!</p>
+        <p>Learning Devops Deployment on Ec2!</p>
       </header>
     </div>
   );
